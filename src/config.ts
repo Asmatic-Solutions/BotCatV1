89d8eb6717dbca3279be62132ace52fc6b8f199d
MrR3set <bairon.pazrobles@gmail.com>
2020-02-21 18:00:32 -0800

    all

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+export let config = {
+    "token" : "NDgwMTQ5MjE3NTQ1OTQ1MDg5.Xjpgng.phxXeEv4p3nJfMO4bdCSFEdtXqI",
+    "prefix": "!",
+    "commands": [
+        "isoncommand",
+        "purge",
+        "join",
+        "leave",
+        "meme",
+        "darkmeme",
+        "nsfw",
+        "cat",
+        "dog",
+        "play",
+        "team",
+        "rr",
+        "lol"
+    ]
+}
+
