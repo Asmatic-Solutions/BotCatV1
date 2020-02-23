9850f552efabbcb73c927c32682c16492f8e372d
MrR3set <bairon.pazrobles@gmail.com>
2020-02-22 20:54:32 -0800

    chapmion info getter + random champ selector

--- src/commands/lol.ts
+++ src/commands/lol.ts
@@ -16,11 +16,48 @@ export default class lol implements IBotCommand{
     }
 
     runThisCommand(args: string[], msgObject: Discord.Message, client:Discord.Client ){
-        this.getWeekly(msgObject)
-        // msgObject.channel.send(``);  
+
+
+        if(args[0]==="w" || args[0]==="week" || args[0]==="weekly"){
+            this.getWeekly(msgObject)
+        }
+
+        if(args[0]==="c" || args[0]==="champ" || args[0]==="champion"){       
+            if(args[1]==="r" || args[1]==="rand" || args[1]==="random"){
+                this.getChampion(msgObject,getChampInfo((Math.floor(Math.random() * lolconfig.lolcfg.champs.length))).name,client);
+             }else{
+                this.getChampion(msgObject,args[1], client);
+             }
+        }
     }
 
-    
+    async getChampion(msgObject:Discord.Message, args:string, client:Discord.Client ){
+        //Get champion object from lolcfg api
+        let champ = lolconfig.lolcfg.champs.find(elem=>elem.name===args);
+        let cType = "";
+        //style it
+        const lolEmbed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setAuthor(`${client.user.username}`, `${client.user.avatarURL}`, 'https://discord.js.org')
+        .setTitle(`${champ.title}, ${champ.name}`)
+        .setURL(`https://leagueoflegends.fandom.com/wiki/${champ.name}`)
+        .setDescription(``)
+        .setThumbnail('https://venturebeat.com/wp-content/uploads/2019/09/league-of-legends.jpg?w=1200&strip=all')
+        .addField('Lore', `${champ.blurb}`)
+        .addField('Difficulty', `${champ.info.difficulty}/10`, true)
+        .setImage(`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champ.name}_0.jpg`)
+        .setTimestamp()
+        .setFooter(`League of legends champ requested by ${msgObject.author.username}`, `${msgObject.author.avatarURL}`);
+
+        champ.tags.forEach(e => {
+            cType += e + ", ";
+        })
+
+        lolEmbed.addField('Role', cType, true);
+        msgObject.channel.send(lolEmbed);  
+    }
+
+
     async getWeekly(msgObject: Discord.Message){
         const api = "https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=" + lolconfig.lolcfg.api;
         let champions: any[] = [];
@@ -39,10 +76,7 @@ export default class lol implements IBotCommand{
         champions.forEach(elem=>{
             lolEmbed.addField(elem.name, elem.title, true)
         })
-
         msgObject.channel.send(lolEmbed);  
-
-
     }
 }
 
